3d5394c0c221bfcaf34ec534463129e045a541d2
EC2 Default User <ec2-user@ip-10-0-1-223.ap-northeast-1.compute.internal>
2016-09-06 13:55:10 +0000

    change image location

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var image = require('./routes/image');
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', index);
-app.get('/image', image);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
+
+var images = [
+  'http://chal-bucket.s3-website-ap-northeast-1.amazonaws.com/g.png',
+  'http://chal-bucket.s3-website-ap-northeast-1.amazonaws.com/c.png',
+  'http://chal-bucket.s3-website-ap-northeast-1.amazonaws.com/p.png',
+];
+
+
 /* GET home page. */
 module.exports = function(req, res) {
-  res.render('index', { title: 'Rock-Paper-Scissors' });
+  res.render('index', { title: 'Rock-Paper-Scissors', image: images[Math.floor(Math.random() * images.length)] });
 };
+
