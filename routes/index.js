51b955b879fdc8240af0149fb25db2cf256af93a
Kenichiro Fukuda <uoritinek@gmail.com>
2016-09-07 11:37:15 +0900

    add hostname information to views

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
-
 var images = [
-  'http://chal-bucket.s3-website-ap-northeast-1.amazonaws.com/g.png',
-  'http://chal-bucket.s3-website-ap-northeast-1.amazonaws.com/c.png',
-  'http://chal-bucket.s3-website-ap-northeast-1.amazonaws.com/p.png',
+    'http://chal-bucket.s3-website-ap-northeast-1.amazonaws.com/g.png',
+    'http://chal-bucket.s3-website-ap-northeast-1.amazonaws.com/c.png',
+    'http://chal-bucket.s3-website-ap-northeast-1.amazonaws.com/p.png',
 ];
 
 
 /* GET home page. */
-module.exports = function(req, res) {
-  res.render('index', { title: 'Rock-Paper-Scissors', image: images[Math.floor(Math.random() * images.length)] });
+module.exports = function (req, res) {
+    res.render('index',
+        {
+            title: 'Rock-Paper-Scissors',
+            image: images[Math.floor(Math.random() * images.length)],
+            footer: 'run at ' + req.hostname
+        });
 };
 
